d388385aabae6a5f7814d751fe596a7015a81889
sofiamatulis <sosomatulis@hotmail.com>
2016-10-27 21:32:53 -0400

    when you sign up you are automatically signed in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    session[:user_id] = @user.id
       if @user.save
+        session[:user_id] = @user.id
         redirect_to photographers_url, notice: "Account sucessfully created!"
       else
         render "new"
