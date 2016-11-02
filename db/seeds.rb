b47a7261f97f618a6558b6ccd1b38becc4f69962
sofiamatulis <sosomatulis@hotmail.com>
2016-11-02 00:01:18 -0400

    trying to fix issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Photographer.create(name: 'Anna', address: '1234 Food Street', phone: '416 123 1234', website: 'www.italianfood.com', category: 'weddings')
+Photographer.create(name: 'Anna', address: '1234 Yonge Street', phone: '416 123 1234', website: 'www.myphotos.com', category: 'weddings')
 
 User.create(name: 'Natalie', phone: '416-787-9898', email: 'natalie@gmail.com', password: '12', password_confirmation: '12')
